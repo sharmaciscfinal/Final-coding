bc992df510b14f26222d7668ae647e57534e48b1
Akash Sharma <aksharma@udel.edu>
2016-05-19 21:55:42 -0400

    BLL & DAL-- DONE

--- RocketBLL/src/test/java/rocketBase/rate_test.java
+++ RocketBLL/src/test/java/rocketBase/rate_test.java
@@ -20,6 +20,12 @@ public class rate_test {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		try {
+			assertEquals(RateBLL.getRate(850), 3.50, 0.01);
+		} catch (RateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	// to test the rate exception	
@@ -28,13 +34,14 @@ public class rate_test {
 		
 		// should throw exception	
 		assertEquals(RateBLL.getRate(100), 0.00, 0.01);
-		
+		assertEquals(RateBLL.getRate(355), 0.00, 0.01);
 	}
 	
 	// to test the getPayment
 	@Test
 	public void PaymentTest() {
 		assertEquals(RateBLL.getPayment(0.04/12, 360, 300000, 0.0, false), 1432.25, 0.01);
+		assertEquals(RateBLL.getPayment(0.03/12, 360, 1000000, 0.0, false),4216.04, 0.01);
 	}
 
 }
--- RocketDAL/src/test/java/rocketBase/Rate_Test.java
+++ RocketDAL/src/test/java/rocketBase/Rate_Test.java
@@ -14,44 +14,22 @@ public class Rate_Test {
 	//DONE - RocketDAL rate_test
 	//		Check to see if a known credit score returns a known interest rate
 	
-	//TODO - RocketDAL rate_test
+	// - RocketDAL rate_test
 	//		Check to see if a RateException is thrown if there are no rates for a given
 	//		credit score
 	@Test
 	public void RateTest() {
 		
 		ArrayList<RateDomainModel> rates = RateDAL.getAllRates();
-		//System.out.println ("Rates size: " + rates.size());
+		System.out.println ("Rates size: " + rates.size());
 		assert(rates.size() > 0);
 		
+		//test to see if you get > 0 rates in the tblRate table
 		assertEquals(rates.get(0).getdInterestRate(), 5.00,  0.01);
 		assertEquals(rates.get(1).getdInterestRate(), 4.50,  0.01);
 		assertEquals(rates.get(2).getdInterestRate(), 4.00,  0.01);
 		assertEquals(rates.get(3).getdInterestRate(), 3.75, 0.01);
 		assertEquals(rates.get(4).getdInterestRate(), 3.50,  0.01);	
-			
-	}
-	//TODO -exception
-	@Test
-	public void RateExceptionTest() {
-		
-		ArrayList<RateDomainModel> rates = RateDAL.getAllRates();
-		System.out.println ("Rates size: " + rates.size());
-		assert(rates.size() > 0);
-		
-		/**
-		 * rates.get(0).setiMinCreditScore(-1);
-		rates.get(1).setiMinCreditScore(-1);
-		rates.get(2).setiMinCreditScore(-1);
-		rates.get(3).setiMinCreditScore(-1);
-		rates.get(4).getiMinCreditScore();
-		rates.
-		**/
-		
-		System.out.println ("Rates : " + rates.get(0).getiMinCreditScore());
-		
-		//assertEquals(rates.get(0).getdInterestRate(), 0.00,  0.01);
-		
 			
 	}
 
